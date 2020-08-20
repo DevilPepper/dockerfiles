a75ce51f3009d73e7b9f562b7d56b5118cd497a9
SupaStuff <StuffQilla@gmail.com>
2020-08-19 23:59:05 -0400

    deleted scripts and added editor stuff

--- scripts/ps1/docker-user.ps1
+++
@@ -1,8 +0,0 @@
-if([System.IO.File]::Exists("~\dockerfiles\tmp\username")){
-        $user = Get-Content ~\dockerfiles\tmp\username -First 1
-}
-else{
-        $user = $env:username.toLower()
-}
-
-echo $user
